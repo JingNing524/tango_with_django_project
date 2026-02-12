5462e42600717aeae8553c6ea6538ccdfc78abb5
Jing Ning Haung <3075433h@student.gla.ac.uk>
2026-02-12 20:02:39 +0000

    10.4

--- rango/views.py
+++ rango/views.py
@@ -36,11 +36,16 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
     
+    request.session.set_test_cookie()
+
     # Render the response and send it back!
     return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     context_dict = {'boldmessage': 'This tutorial has been put together by Jing Ning Huang'}
     return render(request, 'rango/about.html', context=context_dict)
 
